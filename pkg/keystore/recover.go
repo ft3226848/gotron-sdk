da6b23ad14b3e1807d60f5b90eca11f8ec915280
chrise <ft3226841@gmail.com>
2024-06-19 03:22:10 +0800

    11

--- pkg/keystore/recover.go
+++ pkg/keystore/recover.go
@@ -1,1 +1,27 @@
+//go:build !windows
+// +build !windows
+
 package keystore
+
+import (
+	"github.com/fbsobreira/gotron-sdk/pkg/address"
+)
+
+func RecoverPubkey(hash []byte, signature []byte) (address.Address, error) {
+
+	if signature[64] >= 27 {
+		signature[64] -= 27
+	}
+
+	sigPublicKey, err := RecoverPubkey(hash, signature)
+	if err != nil {
+		return nil, err
+	}
+	pubKey, err := UnmarshalPublic(sigPublicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	addr := address.PubkeyToAddress(*pubKey)
+	return addr, nil
+}
